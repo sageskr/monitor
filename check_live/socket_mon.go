6f1223ff974a085f6f28c34f8476d72d3e359232
sunkairong <sunkairong@yidian-inc.com>
2015-10-28 16:25:23 +0800

    增加主食

--- check_live/socket_mon.go
+++ check_live/socket_mon.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	/* 引入各种需求的库*/
 	"bufio"
 	"encoding/json"
 	"fmt"
@@ -13,6 +14,7 @@ import (
 )
 
 type json_data struct {
+	/* 构建falcon可以接收的数据结构 */
 	Endpoint    string  `json:"endpoint"`
 	Metric      string  `json:"metric"`
 	Timestamp   int     `json:"timestamp"`
@@ -22,19 +24,22 @@ type json_data struct {
 	Tags        string  `json:"tags"`
 }
 
-var filename string = os.Args[1]
-var quit chan int
-var chk int = 0
+/* 定义了一些初始化变量，*/
+var filename string = os.Args[1] //定义用户输入的文件名
+var quit chan int                //定义了channel
+var chk int = 0                  //定义了循环的计数器
 var strs []json_data
-var cur_time int
-var url string
+var cur_time int //当前时间戳
+var url string   //url
 
 func add_json(in json_data) []json_data {
+	// 把json格式的文本导入json串中
 	strs = append(strs, in)
 	return strs
 }
 
 func decode_json(in_value float64, in_tag string, in_time int) json_data {
+	// 解析成json格式的数据，本样例中因为是存活监控，因此是GAUGE类型，大部分项目为固定项目。
 	j_tags := "ip=" + in_tag
 	s := json_data{
 		Endpoint:    `test_localhost`,
@@ -49,6 +54,7 @@ func decode_json(in_value float64, in_tag string, in_time int) json_data {
 }
 
 func checkError(err error) {
+	// 错误检查
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
 		os.Exit(1)
@@ -56,6 +62,7 @@ func checkError(err error) {
 }
 
 func mon(ip string, in_time int) {
+	// 根据用户输入的IP，建立TCP连接，超时时间为10秒，如正常返回1，异常为0
 	var ip_port string = ip
 	timeOut := time.Duration(10) * time.Second
 	conn, err := net.DialTimeout("tcp", ip_port, timeOut)
@@ -75,6 +82,7 @@ func mon(ip string, in_time int) {
 	defer conn.Close()
 }
 func main() {
+	//初始化时间戳
 	cur_time := int(time.Now().Unix())
 	fo, err := os.Open(filename)
 	if err != nil {
@@ -92,10 +100,12 @@ func main() {
 		chk += 1
 		go mon(s_line, cur_time)
 	}
+	//86 - 102 以行读入文件，并简历goruntime进行检查
 	quit = make(chan int, chk)
 	for i := 0; i < chk; i++ {
 		<-quit
 	}
+	//检查chan是否结束
 	b, _ := json.Marshal(strs)
 	// os.Stdout.Write(b)
 	url = "http://127.0.0.1:1988/v1/push"
@@ -104,5 +114,6 @@ func main() {
 		fmt.Print(err)
 	}
 	defer resp.Body.Close()
+	// 108 - 116 push数据
 }
 
